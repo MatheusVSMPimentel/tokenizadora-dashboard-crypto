77a62eced1d52962411065528145997f8376598e
Matheus Pimentel <matheusvsm.pimentel@hotmail.com>
2025-02-27 23:03:40 -0300

    feature: using env file to set variables from database criar e organizar projeto User  nestjs #1

--- dashboard-user-api/src/app.module.ts
+++ dashboard-user-api/src/app.module.ts
@@ -1,19 +1,30 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './domain/users/users.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import databaseConfig from './infrastructure/config/database.config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'usrclient',
-      password: 'SenhaDoUsuario123',
-      database: "userclient",
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true, //just for experimental purpose
+    ConfigModule.forRoot({
+      isGlobal: true, // Torna as variáveis de ambiente acessíveis globalmente
     }),
-    UsersModule]
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService): TypeOrmModuleOptions => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [__dirname + '/**/*.entity.js' ], // '/../**/*.entity{.ts,.js}'
+        synchronize: true, // just for experimental purpose
+      }),
+    }),
+    UsersModule,
+  ],
+    
 })
 export class AppModule {}
