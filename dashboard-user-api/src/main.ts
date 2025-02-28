c78144d2ed7986067aea1871d317a4557567f7c4
Matheus Pimentel <matheusvsm.pimentel@hotmail.com>
2025-02-28 16:09:11 -0300

    chore: standard vue files [Front end] criar pagina dashboard logado #15

--- dashboard-user-api/src/main.ts
+++ dashboard-user-api/src/main.ts
@@ -6,5 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({transform: true, whitelist: true}))
   await app.listen(process.env.PORT ?? 3000);
+  app.enableCors({
+    origin: 'http://localhost:5173'
+  });
 }
+
 bootstrap();
